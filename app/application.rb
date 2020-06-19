2f5a4d45f0dd4decd5f655513bbb7cc3dd45807f
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-19 00:54:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,15 +16,20 @@ class Application
       resp.write handle_search(search_term)
 
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write("#{item}\n")
+      if @@cart.empty?
+        resp.write("Your cart is empty")
+      else
+        @@cart.each do |item|
+          resp.write("#{item}\n")
+        end
       end
     elsif req.path.match(/add/)
       cart_item = req.params['q']
       if @@items.include?(cart_item)
+        resp.write("added #{cart_item}")
         @@cart << cart_item
       else
-        resp.write("Item not found!")
+        resp.write("We don't have that item")
       end
     else
       resp.write "Path Not Found"
